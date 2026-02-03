4e14c43c6b5e0475683d47556dbb18f025a9b3bc
durgeshh04 <durgeshchaudhari199@gmail.com>
2026-02-03 23:38:30 +0530

    schema structure created

--- src/db/schema/hotels.js
+++ src/db/schema/hotels.js
@@ -1,4 +1,4 @@
-import { serial, varchar, integer } from "drizzle-orm/pg-core";
+import { serial, varchar, integer, json, decimal } from "drizzle-orm/pg-core";
 import { pgTable, foreignKey } from "drizzle-orm/pg-core";
 import { users } from "./users";
 
@@ -8,6 +8,16 @@ export const hotels = pgTable(
     id: serial("id").primaryKey(),
     owner_id: integer("owner_id").notNull(),
     name: varchar("name", { length: 255 }).notNull(),
+    description: varchar("description").notNull(),
+    city: varchar("city").notNull(),
+    country: varchar("country").notNull(),
+    amenities: json("amenities"),
+    rating: decimal(2, 1).default(0.0),
+    total_reviews: integer("total_reviews").default(0),
+    created_at: timestamp("created_at").defaultNow().notNull(),
+    updated_at: timestamp("updated_at")
+      .defaultNow()
+      .$onUpdate(() => new Date()),
   },
   (table) => ({
     ownerFk: foreignKey({
